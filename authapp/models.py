3fc4a931f14ef8f8648499337a79af19a0e0a73a
Dima <hodas.work@gmail.com>
2023-02-11 20:30:06 +0300

    REST API

--- authapp/models.py
+++ authapp/models.py
@@ -1,2 +1,10 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+
+class Users(AbstractBaseUser):
+    username = models.CharField(max_length=64, unique=True)
+    first_name = models.CharField( max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "username"
