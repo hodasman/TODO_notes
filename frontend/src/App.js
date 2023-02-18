25fa44de763e93f64e5333ffd97b2682d61a3a35
Dima <hodas.work@gmail.com>
2023-02-18 21:53:53 +0300

    homework_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from './components/Footer.js'
+import Navbar from './components/Menu.js'
+import Userlist from './components/User.js'
+
+
+const DOMAIN = 'http://127.0.0.1:8000/api/'
+const get_url = (url => `${DOMAIN}${url}`)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'navbarItems': [
+        {name: 'users', href: '/todo'},
+      ],
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(get_url('users')).then(response => {
+      this.setState({users: response.data})
+    }).catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Navbar navbarItems={this.state.navbarItems}/>
+        </header>
+        <div className='container'>
+          <Userlist users={this.state.users}/>
+        </div>
+        <Footer/>
+      </div>
+    
+    )
+  }
 }
+
 
 export default App;
