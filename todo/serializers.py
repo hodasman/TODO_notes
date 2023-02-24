bbdf0d5b59fb40f6414acd0fb34eefae9fb9979f
Dima <hodas.work@gmail.com>
2023-02-24 19:59:32 +0300

    add serializers.py

---
+++ todo/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer, StringRelatedField
+from .models import Project, TODO
+from authapp.serializers import UsersModelSerializer
+
+class ProjectModelSerializer(ModelSerializer):
+    users = StringRelatedField(many=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class TODOModelSerializer(ModelSerializer):
+    project = ProjectModelSerializer()
+    
+    class Meta:
+        model = Project
+        fields = '__all__'
