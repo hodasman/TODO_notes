399720ff8148c696a8a7c4c558bfb91bf89d4f16
Dima <hodas.work@gmail.com>
2023-03-09 21:41:38 +0300

    add api.py

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,12 @@ from rest_framework.routers import DefaultRouter
 from authapp.views import UsersCustomViewSet
 from todo.views import ProjectModelViewSet, TODOViewSet
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 
 router = DefaultRouter()
 router.register('users', UsersCustomViewSet)
@@ -30,5 +36,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
