9ef3f7ab903a6dbc599ab8c530b181dd342d2291
Dima <hodas.work@gmail.com>
2023-06-11 21:18:09 +0300

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -92,15 +92,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'library',
-        # 'USER': 'dante',
-        # 'PASSWORD': 'dante123456',
-        # 'HOST': 'db',
-        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'dante',
+        'PASSWORD': 'dante123456',
+        'HOST': 'db',
+        'PORT': '5432',
 
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
